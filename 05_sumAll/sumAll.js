96709c056d35720cdd524997ad5dfe1f18fce44a
vagabond <benclary135@gmail.com>
2022-11-18 12:19:42 -0500

    Complete lesson 05.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    const highNum = Math.max(num1, num2);
+    const lowNum = Math.min(num1, num2);
+    let total = lowNum;
+    if (typeof num1 !== "number" || typeof num2 !== "number" || 
+      Math.sign(num1) == -1 || Math.sign(num2) == -1 ) {
+        return "ERROR";
+    } else {
+        for (let i = (lowNum + 1); i <= (highNum); i++) {
+            total += i;
+        }
+        return total;
+    }
 };
 
 // Do not edit below this line
