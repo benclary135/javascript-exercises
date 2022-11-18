547e719745d62a330b8b2f820d29f703026a84c2
vagabond <benclary135@gmail.com>
2022-11-17 21:41:27 -0500

    Start lesson 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(startArray, ...arguments) {
+    const toRemove = Array.from(arguments);
+    for (i = 0; i < startArray.length; i++) {
+        for (x = 0; x < toRemove.length; x++) {
+            if (toRemove[x] === startArray[i]) {
+                
+            }
+        }
+    }
 };
 
 // Do not edit below this line
