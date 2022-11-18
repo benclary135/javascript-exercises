72755f2bd3430c44c2f39ce21565c8d9a0b6a0f1
vagabond <benclary135@gmail.com>
2022-11-17 20:52:43 -0500

    Complete lesson 03.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const initialArray = Array.from(string);
+    const reverseArray = [];
+    if (string == '') {
+        return '';
+    } else {
+        for (i=0; i<(string.length)+1; i++){
+            reverseArray[i] = initialArray[(string.length)-i];
+        }
+        return reverseArray.join('');
+    }
 };
 
 // Do not edit below this line
