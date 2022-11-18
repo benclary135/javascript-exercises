3bddb2fa71fadacd4d802c309233e63f9e82b355
vagabond <benclary135@gmail.com>
2022-11-17 20:29:44 -0500

    Complete lesson 02.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    const stagingGround = [string];
+    if(num === 0) {
+        return '';
+    } else if(num < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = 0; i < (num - 1); i++) {
+            stagingGround.push(string);
+        }
+        return stagingGround.join('');
+    }
+}
 
-};
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
