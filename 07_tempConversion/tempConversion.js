210ce61fcbe2edce653eeb4ccd7aebddd495d597
vagabond <benclary135@gmail.com>
2022-11-18 20:27:19 -0500

    Complete lesson 07.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let convertedC = (tempF-32) * (5/9);
+  convertedC = Math.round(convertedC *10) / 10;
+  return convertedC
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let convertedF = (tempC * (9/5) + 32);
+  convertedF = Math.round(convertedF * 10) / 10;
+  return convertedF;
 };
+
+
+
 
 // Do not edit below this line
 module.exports = {
